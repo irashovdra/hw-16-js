92ecaec21a0dfd059e8f31431effcf57e0c11879
irashovdra <109846925+irashovdra@users.noreply.github.com>
2023-12-29 18:26:01 -0600

    exercise 3

--- exercise3.js
+++ exercise3.js
@@ -1,14 +1,22 @@
-const findBestEmployee = function (employees) {
-    for (const employee of employees) {
-        
-    }
+const employees = {
+  Denys: 28,
+  Katia: 9,
+  Dima: 13,
+  Masha: 54,
 };
 
-console.log(
-  findBestEmployee({
-    Denys: 28,
-    Katia: 9,
-    Dima: 13,
-    Masha: 54,
-  })
-);
+function findBestEmployee(employees) {
+  let bestEmployee = "";
+  let maxTasks = 0;
+
+  for (const [employee, tasks] of Object.entries(employees)) {
+    if (tasks > maxTasks) {
+      maxTasks = tasks;
+      bestEmployee = employee;
+    }
+  }
+
+  return `Найпродуктивніший співробітник: ${bestEmployee}`;
+}
+
+console.log(findBestEmployee(employees));
---
+++ exercise4.js
@@ -0,0 +1,5 @@
+
+
+function countTotalSalary(employees) {
+
+}
